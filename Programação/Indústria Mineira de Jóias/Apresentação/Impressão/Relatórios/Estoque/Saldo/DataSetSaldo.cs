02e649f37dff6f3b37462ac01aa26ff86e9d3200
André Pontes <andrepontes@gmail.com>
2015-08-08 11:35:33 -0300

    complemento do commit anterior.

---
+++ Programação/Indústria Mineira de Jóias/Apresentação/Impressão/Relatórios/Estoque/Saldo/DataSetSaldo.cs
@@ -0,0 +1,9 @@
+﻿namespace Apresentação.Impressão.Relatórios.Estoque.Saldo
+{
+    public partial class DataSetSaldo
+    {
+        partial class InformaçõesDataTable
+        {
+        }
+    }
+}
