4405c3ddf699ffae8e1478a8a537910e85502430
André Pontes <andrepontes@gmail.com>
2015-08-15 22:31:38 -0300

    Combobox de fornecedor

---
+++ Programação/Indústria Mineira de Jóias/Apresentação/Fornecedor/ComboboxFornecedor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Apresentação.Fornecedor
+{
+    public partial class ComboboxFornecedor : UserControl
+    {
+        public ComboboxFornecedor()
+        {
+            InitializeComponent();
+        }
+
+        public void Carregar()
+        {
+            if (!bg.IsBusy)
+                bg.RunWorkerAsync();
+        }
+
+        private void bg_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<Entidades.Fornecedor> fornecedores = Entidades.Fornecedor.ObterFornecedores();
+
+            e.Result = fornecedores;
+        }
+
+        private void bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            List<Entidades.Fornecedor> fornecedores = (List<Entidades.Fornecedor>) e.Result;
+            Entidades.Fornecedor[] fornecedoresVetor = fornecedores.ToArray();
+
+            comboBox.Items.Clear();
+            comboBox.Items.AddRange(fornecedoresVetor);
+        }
+    }
+}
