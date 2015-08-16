7464867c86fb6e9ca01c9ed9edee07f67aa7b21d
André Pontes <andrepontes@gmail.com>
2015-08-15 22:31:38 -0300

    Combobox de fornecedor
    Former-commit-id: d2fe18e8c96d4ed0b1b97b0cd016e9c37f08c847 [formerly 4405c3ddf699ffae8e1478a8a537910e85502430]
    Former-commit-id: 343a1a67f15ec67e781a659cc5d0e3bfc5a6a990

---
+++ Programação/Indústria Mineira de Jóias/Apresentação/Fornecedor/ComboboxFornecedor.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Apresentação.Fornecedor
+{
+    partial class ComboboxFornecedor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.bg = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(0, 0);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(345, 21);
+            this.comboBox.TabIndex = 0;
+            // 
+            // bg
+            // 
+            this.bg.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bg_DoWork);
+            this.bg.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bg_RunWorkerCompleted);
+            // 
+            // ComboboxFornecedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox);
+            this.Name = "ComboboxFornecedor";
+            this.Size = new System.Drawing.Size(345, 23);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.ComponentModel.BackgroundWorker bg;
+    }
+}
