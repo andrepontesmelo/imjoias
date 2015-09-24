d68bee40827bdaef5b0499687c492eb89838dbe1
André Pontes <andrepontes@gmail.com>
2015-09-24 08:07:45 -0300

    Cheque com prorrogação mostrava prorrogação ao invés de vencimento na lista de pagamentos, na coluna vencimento
    Former-commit-id: 5c3f8374a0071427bb39f3ad4eed15d39bfb020f [formerly 7d7ea730f318510b62e52f6dd19cdf2d00ca857b]
    Former-commit-id: fc4b0dd65821dcb44165617daa6af065fc0d9009

--- Programação/Indústria Mineira de Jóias/Entidades/Acerto/Sumário/SumárioTotalAcertoItem.cs
+++ Programação/Indústria Mineira de Jóias/Entidades/Acerto/Sumário/SumárioTotalAcertoItem.cs
@@ -28,6 +28,8 @@ namespace Entidades.Acerto.Sumário
 
         }
 
+
+
         public static List<SumárioTotalAcertoItem> Obter(AcertoConsignado entidade)
         {
             StringBuilder cmd = new StringBuilder();
@@ -35,6 +37,50 @@ namespace Entidades.Acerto.Sumário
             double cotação = entidade.Cotação.HasValue ? entidade.Cotação.Value : 0;
             ulong códigoAcerto = entidade.Código;
 
+
+/*
+set @acerto := 18762;
+set @cotacao := (select cotacao from acertoconsignado where codigo = @acerto);
+
+select 'Saida' as tipo, m.depeso, round(sum(indice*quantidade),2) as indice,
+round(sum(i.peso*quantidade),2) as peso,
+round(sum(indice*quantidade*@cotacao),2) as preco,
+sum(quantidade) as qtd from saida s
+join saidaitem i on s.codigo=i.saida
+join mercadoria m on i.referencia=m.referencia
+where acerto=@acerto
+group by m.depeso
+
+UNION
+
+select 'Retorno' as tipo, m.depeso, round(sum(indice*quantidade),2) as indice,
+round(sum(i.peso*quantidade),2) as peso,
+round(sum(indice*quantidade*@cotacao),2) as preco,
+sum(quantidade) as qtd
+ from retorno r
+join retornoitem i on r.codigo=i.retorno
+join mercadoria m on i.referencia=m.referencia
+where acerto=@acerto
+group by m.depeso
+
+UNION
+
+select 'Venda' as tipo, depeso, round(sum(indice),2) as indice, round(sum(peso),2) as peso, round(sum(preco),2) as preco, sum(qtd) as qtd from
+(select m.referencia, m.depeso as depeso, round(sum(indice*quantidade),2) as indice,  round(sum(i.peso*quantidade),2) as peso, round(sum(indice*quantidade*@cotacao),2) as preco, sum(quantidade) as qtd
+from venda v
+join vendaitem i on v.codigo=i.venda
+join mercadoria m on i.referencia=m.referencia
+where acerto=@acerto
+group by m.referencia
+UNION
+select m.referencia, m.depeso as depeso, round(sum(-1*indice*quantidade),2) as indice,  round(sum(-1*i.peso*quantidade),2) as peso, round(sum(-1*indice*quantidade*@cotacao),2) as preco, round(sum(-1*quantidade),2) as qtd
+from venda v
+join vendadevolucao i on v.codigo=i.venda
+join mercadoria m on i.referencia=m.referencia
+where acerto=@acerto
+group by m.referencia) cc
+group by depeso
+*/
 
             cmd.Append(" select 'Saida' as tipo, m.depeso, round(sum(indice*quantidade),2) as indice,  ");
             cmd.Append(" round(sum(i.peso*quantidade),2) as peso, ");
