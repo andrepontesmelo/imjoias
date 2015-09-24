2b1520b56ba2461a54235c3ca157762fecf82350
André Pontes <andrepontes@gmail.com>
2015-09-24 07:57:28 -0300

    Correção da SQL do sumário: faltava um group by dentro da união da select de venda e devolução, o que deixava incorreto a parte das vendas
    Former-commit-id: 9320cf2a0f9042ee4e16a56ce81b90433205b5d3 [formerly c006e135f5e8d64e14c7d459b50845c6bce872b6]
    Former-commit-id: 55fb0ce39dccbe3eabde5ce6677a76c5e57ba62b

--- Programação/Indústria Mineira de Jóias/Entidades/Acerto/Sumário/SumárioTotalAcertoItem.cs
+++ Programação/Indústria Mineira de Jóias/Entidades/Acerto/Sumário/SumárioTotalAcertoItem.cs
@@ -34,25 +34,53 @@ namespace Entidades.Acerto.Sumário
 
             double cotação = entidade.Cotação.HasValue ? entidade.Cotação.Value : 0;
             ulong códigoAcerto = entidade.Código;
-            
-            cmd.Append(" select 'Saida' as tipo, m.depeso, round(sum(indice*quantidade),2) as indice,   round(sum(i.peso*quantidade),2) as peso,  round(sum(indice*quantidade* ");
+
+
+            cmd.Append(" select 'Saida' as tipo, m.depeso, round(sum(indice*quantidade),2) as indice,  ");
+            cmd.Append(" round(sum(i.peso*quantidade),2) as peso, ");
+            cmd.Append(" round(sum(indice*quantidade* ");
             cmd.Append(DbTransformar(cotação));
-            cmd.Append(" ),2) as preco,  sum(quantidade) as qtd   from saida s   join saidaitem i on s.codigo=i.saida  join mercadoria m on i.referencia=m.referencia  where acerto=");
+            cmd.Append(" ),2) as preco, ");
+            cmd.Append(" sum(quantidade) as qtd from saida s  ");
+            cmd.Append(" join saidaitem i on s.codigo=i.saida ");
+            cmd.Append(" join mercadoria m on i.referencia=m.referencia ");
+            cmd.Append(" where acerto= ");
             cmd.Append(DbTransformar(códigoAcerto));
-            cmd.Append(" group by m.depeso  UNION   select 'Retorno' as tipo, m.depeso, round(sum(indice*quantidade),2) as indice,   round(sum(i.peso*quantidade),2) as peso,  round(sum(indice*quantidade*");
+            cmd.Append(" group by m.depeso UNION  select 'Retorno' as tipo, m.depeso, round(sum(indice*quantidade),2) as indice,  ");
+            cmd.Append(" round(sum(i.peso*quantidade),2) as peso, ");
+            cmd.Append(" round(sum(indice*quantidade* ");
             cmd.Append(DbTransformar(cotação));
-            cmd.Append(" ),2) as preco,  sum(quantidade) as qtd    from retorno r   join retornoitem i on r.codigo=i.retorno  join mercadoria m on i.referencia=m.referencia  where acerto=");
+            cmd.Append(" ),2) as preco, ");
+            cmd.Append(" sum(quantidade) as qtd  ");
+            cmd.Append("  from retorno r  ");
+            cmd.Append(" join retornoitem i on r.codigo=i.retorno ");
+            cmd.Append(" join mercadoria m on i.referencia=m.referencia ");
+            cmd.Append(" where acerto= ");
             cmd.Append(DbTransformar(códigoAcerto));
-            cmd.Append(" group by m.depeso  UNION   select 'Venda' as tipo, depeso, round(sum(indice),2) as indice, round(sum(peso),2) as peso, round(sum(preco),2) as preco, sum(qtd) as qtd from   (select m.depeso as depeso, indice*quantidade as indice,  i.peso*quantidade as peso, indice*quantidade*");
+            cmd.Append(" group by m.depeso UNION select 'Venda' as tipo, depeso, round(sum(indice),2) as indice, round(sum(peso),2) as peso, round(sum(preco),2) as preco, sum(qtd) as qtd from  ");
+            cmd.Append(" (select m.referencia, m.depeso as depeso, round(sum(indice*quantidade),2) as indice,  round(sum(i.peso*quantidade),2) as peso, round(sum(indice*quantidade* ");
             cmd.Append(DbTransformar(cotação));
-            cmd.Append(" as preco, quantidade as qtd   from venda v   join vendaitem i on v.codigo=i.venda  join mercadoria m on i.referencia=m.referencia  where acerto=");
+            cmd.Append(" ),2) as preco, sum(quantidade) as qtd  ");
+            cmd.Append(" from venda v  ");
+            cmd.Append(" join vendaitem i on v.codigo=i.venda ");
+            cmd.Append(" join mercadoria m on i.referencia=m.referencia ");
+            cmd.Append(" where acerto= ");
             cmd.Append(DbTransformar(códigoAcerto));
-            cmd.Append(" UNION  select m.depeso as depeso, -1*indice*quantidade as indice,  -1*i.peso*quantidade as peso, -1*indice*quantidade*");
+            cmd.Append(" group by m.referencia ");
+            cmd.Append(" UNION ");
+            cmd.Append(" select m.referencia, m.depeso as depeso, round(sum(-1*indice*quantidade),2) as indice,  round(sum(-1*i.peso*quantidade),2) as peso, round(sum(-1*indice*quantidade* ");
             cmd.Append(DbTransformar(cotação));
-            cmd.Append(" as preco, -1*quantidade as qtd   from venda v   join vendadevolucao i on v.codigo=i.venda  join mercadoria m on i.referencia=m.referencia  where acerto=");
-            cmd.Append(DbTransformar(códigoAcerto));            
-            cmd.Append(" ) cc   group by depeso     ");
-            
+            cmd.Append(" ),2) as preco, round(sum(-1*quantidade),2) as qtd  ");
+            cmd.Append(" from venda v  ");
+            cmd.Append(" join vendadevolucao i on v.codigo=i.venda ");
+            cmd.Append(" join mercadoria m on i.referencia=m.referencia ");
+            cmd.Append(" where acerto= ");
+            cmd.Append(DbTransformar(códigoAcerto));
+            cmd.Append(" group by m.referencia) cc ");
+            cmd.Append(" group by depeso ");
+
+
+
             return Mapear<SumárioTotalAcertoItem>(cmd.ToString());
         }
 
