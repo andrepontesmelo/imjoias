a8b2f6b0b07ff76a35f547212743333c3b0e05de
André Pontes <andrepontes@gmail.com>
2015-10-01 23:30:27 -0300

    Correção na seleção do índice de varejo nas digitações. Agora é escolhido o índice da tabela, antes era escolhido o primeiro valor das escolhas, podendo não ser o vigente.

--- Programação/Indústria Mineira de Jóias/Apresentação/Financeiro/DigitaçãoComum.cs
+++ Programação/Indústria Mineira de Jóias/Apresentação/Financeiro/DigitaçãoComum.cs
@@ -316,20 +316,24 @@ namespace Apresentação.Financeiro
                 if (!listaÍndicesArredondados.Contains(mercadoria.ÍndiceArredondado))
                     listaÍndicesArredondados.Add(mercadoria.ÍndiceArredondado);
 
+                /* Código temporário:
+                 * 
+                 * a seguinte linha desliga a janela de escolha de índice para o Varejo.
+                 * Será escolhido o valor da mercadoria em real (índice arrendondado).
+                 * 
+                 * Para novos acertos, o indice na saída já será o valor em real e então
+                 * todo este código poderá ser removido.
+                 * 
+                 */
+                if (mercadoria.TabelaPreço.Nome == "Varejo")
+                {
+                    mercadoria.Índice = mercadoria.ÍndiceArredondado;
+                    return;
+                }
+
                 // Neste ponto a listaÍndices tem um ou mais elementos.
                 if ((modoJanela == ModoJanelaÍndice.MostrarSempre || listaÍndicesArredondados.Count > 1)
-                    && (modoJanela != ModoJanelaÍndice.NuncaMostrarNãoAlterandoÍndice)
-
-                    /* Código temporário:
-                     * 
-                     * a seguinte linha desliga a janela de escolha de índice para o Varejo.
-                     * Será escolhido o valor da mercadoria em real (índice arrendondado).
-                     * 
-                     * Para novos acertos, o indice na saída já será o valor em real e então
-                     * todo este código poderá ser removido.
-                     * 
-                     */
-                    && mercadoria.TabelaPreço.Nome != "Varejo")
+                    && (modoJanela != ModoJanelaÍndice.NuncaMostrarNãoAlterandoÍndice))
                 {
                     JanelaEscolhaÍndice j = new JanelaEscolhaÍndice();
                     j.CarregarÍndices(listaÍndicesArredondados, mercadoria.ÍndiceArredondado, mercadoria, entidade as RelacionamentoAcerto);
