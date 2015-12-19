4df2a435bb05623476bfc53addcd90230868e687
André Pontes <andrepontes@gmail.com>
2015-12-19 09:21:24 -0200

    Usabilidade: ESC para fechar janela de escolha de tabela ao registrar venda.

--- Programação/Indústria Mineira de Jóias/Apresentação/Financeiro/EscolherTabela.Designer.cs
+++ Programação/Indústria Mineira de Jóias/Apresentação/Financeiro/EscolherTabela.Designer.cs
@@ -99,7 +99,6 @@
             this.btnOK.TabIndex = 5;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
-            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // botãoLiberarRecurso
             // 
