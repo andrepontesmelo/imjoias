9d165380510800be62f13bc8b2e97660edafb84b
André Pontes <andrepontes@gmail.com>
2015-12-20 13:49:01 -0200

    Usabilidade: Deixando OK ser o botão padrão na janela de escolha de índice
    Former-commit-id: 34ea7e0d7c6bce1639963e27b5bd153740dc0758 [formerly 046e320deb33c17b198bfd8e45f421244e389d1e]
    Former-commit-id: 25a6d6058ab5d3fef4b73656abbff5b4080db8e8

--- Programação/Indústria Mineira de Jóias/Apresentação/Financeiro/JanelaEscolhaÍndice.Designer.cs
+++ Programação/Indústria Mineira de Jóias/Apresentação/Financeiro/JanelaEscolhaÍndice.Designer.cs
@@ -149,6 +149,7 @@
             // 
             // JanelaEscolhaÍndice
             // 
+            this.AcceptButton = this.btnOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancelar;
