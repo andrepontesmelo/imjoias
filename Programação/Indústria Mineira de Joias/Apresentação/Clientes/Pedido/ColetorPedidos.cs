ab40dd47706b9be0707fabb54f053bdd1354f434
André Pontes <andrepontes@gmail.com>
2016-05-31 09:08:02 -0300

    Código limpo.

--- Programação/Indústria Mineira de Joias/Apresentação/Clientes/Pedido/ColetorPedidos.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Clientes/Pedido/ColetorPedidos.cs
@@ -1,42 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Apresentação.Formulários.Consultas;
+﻿using Apresentação.Formulários.Consultas;
 
 namespace Apresentação.Atendimento.Clientes.Pedido
 {
-    class ColetorPedidos : Apresentação.Formulários.Consultas.Coletor
+    class ColetorPedidos : Coletor
     {
-		// Constantes
-        private readonly int    padrãoLimiteMínimo = 7;
-        private readonly int?   padrãoLimiteMáximo = 80;
-        private readonly int    padrãoDemoraMáximaMs = 400;
+        private readonly int padrãoLimiteMínimo = 7;
+        private readonly int? padrãoLimiteMáximo = 80;
+        private readonly int padrãoDemoraMáximaMs = 400;
 
-        //// Atributos
-        //private bool			            funcionários = false;	// Coletar somente funcionários
-        //private bool			            vendedores = false;	    // Coletar somente vendedores
-        private ControladorLimiteColetor    controladorLimite;      // Controla o limite dos nomes
+        private ControladorLimiteColetor controladorLimiteNomes;
 
-		// Delegações
         public delegate void RecuperaçãoPedidosDelegate(Entidades.PedidoConserto.Pedido[] pedidos);
         private RecuperaçãoPedidosDelegate recuperaçãoPedidos;
 
         public ColetorPedidos(RecuperaçãoPedidosDelegate recuperação)
 		{
-			// Atribui tratamento de recuperação
             this.recuperaçãoPedidos = recuperação;
-
-            controladorLimite = new ControladorLimiteColetor(padrãoLimiteMínimo, padrãoLimiteMáximo, padrãoDemoraMáximaMs);
+            controladorLimiteNomes = new ControladorLimiteColetor(padrãoLimiteMínimo, padrãoLimiteMáximo, padrãoDemoraMáximaMs);
 		}
 
 		protected override void Recuperar(string chave)
 		{
-			Entidades.PedidoConserto.Pedido [] pedidos;
-
-            controladorLimite.CronometrarInicioObter();
-            pedidos = Entidades.PedidoConserto.Pedido.Obter(chave, controladorLimite.LimiteDinâmico);
-            recuperaçãoPedidos(pedidos);
-            controladorLimite.CronometrarFimObter();
+            controladorLimiteNomes.CronometrarInicioObter();
+            recuperaçãoPedidos(Entidades.PedidoConserto.Pedido.Obter(chave, controladorLimiteNomes.LimiteDinâmico));
+            controladorLimiteNomes.CronometrarFimObter();
 		}
     }
 }
