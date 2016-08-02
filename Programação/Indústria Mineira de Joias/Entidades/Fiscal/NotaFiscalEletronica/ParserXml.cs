ec12580e9c15266adfb12cb70e6fd4f8f3f14b4f
André Pontes <andrepontes@gmail.com>
2016-08-02 07:55:30 -0300

    Fiscal - parser xml - quantidade venda item - red

---
+++ Programação/Indústria Mineira de Joias/Entidades/Fiscal/NotaFiscalEletronica/ParserXml.cs
@@ -0,0 +1,22 @@
+﻿namespace Entidades.Fiscal.NotaFiscalEletronica
+{
+    public class ParserXml
+    {
+        public ParserXml(string arquivo)
+        {
+        }
+
+        public int QuantidadeVendaItem
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public static ParserXml LerArquivo(string arquivo)
+        {
+            return new ParserXml(arquivo);
+        }
+    }
+}
---
+++ Programação/Indústria Mineira de Joias/EntidadesTest/Fiscal/NotaFiscalEletronica/ParserXmlTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades.Fiscal.NotaFiscalEletronica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace Entidades.Fiscal.NotaFiscalEletronica.Tests
+{
+    [TestClass()]
+    public class ParserXmlTests
+    {
+        public static string ARQUIVO_ENTRADA_RELATIVO = @"\Arquivos\nfe.xml";
+        public static string ARQUIVO_ENTRADA = Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().Location)).FullName).FullName + ARQUIVO_ENTRADA_RELATIVO;
+
+        [TestMethod()]
+        public void LerArquivoTest()
+        {
+            Assert.AreEqual(ParserXml.LerArquivo(ARQUIVO_ENTRADA).QuantidadeVendaItem, 28);
+        }
+    }
+}
