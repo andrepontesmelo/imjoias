343b5fd4bda1afb99930f1e45dbc28102e8dcea2
André Pontes <andrepontes@gmail.com>
2016-08-09 22:01:14 -0300

    Pesquisa avançada de mercadoria: evitando que janela de query se feche.

--- Programação/Indústria Mineira de Joias/Apresentação/Financeiro/PesquisaMercadoria.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Financeiro/PesquisaMercadoria.cs
@@ -35,44 +35,69 @@ namespace Apresentação.Financeiro
             PesquisaMercadoriaResultado resultado;
             Entidades.Mercadoria.Mercadoria[] mercadorias;
 
-            UseWaitCursor = true;
+            mercadorias = CarregarMercadorias();
 
+            resultado = new PesquisaMercadoriaResultado(mercadorias, cmbTabela.Seleção, txtCotação.Cotação);
+            DialogResult = DialogResult.OK;
+
+            resultado.Show();
+        }
+
+        private Entidades.Mercadoria.Mercadoria[] CarregarMercadorias()
+        {
+            Entidades.Mercadoria.Mercadoria[] mercadorias;
+            UseWaitCursor = true;
             using (Aguarde aguarde = new Aguarde("Pesquisando...", 4))
             {
                 aguarde.Abrir();
 
                 using (ControlePesquisaMercadoria controle = new ControlePesquisaMercadoria(cmbTabela.Seleção))
                 {
-                    aguarde.Passo();
-
-                    if (txtValorMáximo.Double > 0 && txtCotação.Cotação != null)
-                    {
-                        controle.FiltrarÍndice(txtValorMáximo.Double / txtCotação.Cotação.Valor);
-                    }
-
-                    aguarde.Passo();
-                    if (chkTipo.CheckedItems.Count > 0)
-                        controle.FiltrarTipos((MercadoriaTipo[])(new ArrayList(chkTipo.CheckedItems).ToArray(typeof(MercadoriaTipo))));
-
-                    aguarde.Passo();
-                    if (chkMetal.CheckedItems.Count > 0)
-                        controle.FiltrarMetais((Metal[])(new ArrayList(chkMetal.CheckedItems).ToArray(typeof(Metal))));
-
-                    aguarde.Passo();
-                    if (chkPedras.CheckedItems.Count > 0)
-                        controle.FiltrarPedras((Pedra[])(new ArrayList(chkPedras.CheckedItems).ToArray(typeof(Pedra))));
-
+                    Filtrar(aguarde, controle);
                     aguarde.Passo();
 
                     mercadorias = controle.ObterMercadorias();
                 }
             }
 
-            resultado = new PesquisaMercadoriaResultado(mercadorias, cmbTabela.Seleção, txtCotação.Cotação);
-            DialogResult = DialogResult.OK;
-            Close();
+            UseWaitCursor = false;
+            return mercadorias;
+        }
 
-            resultado.Show();
+        private void Filtrar(Aguarde aguarde, ControlePesquisaMercadoria controle)
+        {
+            FiltrarValorMáximo(aguarde, controle);
+            FiltrarTipos(aguarde, controle);
+            FiltrarMetais(aguarde, controle);
+            FiltrarPedras(aguarde, controle);
+        }
+
+        private void FiltrarPedras(Aguarde aguarde, ControlePesquisaMercadoria controle)
+        {
+            aguarde.Passo();
+            if (chkPedras.CheckedItems.Count > 0)
+                controle.FiltrarPedras((Pedra[])(new ArrayList(chkPedras.CheckedItems).ToArray(typeof(Pedra))));
+        }
+
+        private void FiltrarMetais(Aguarde aguarde, ControlePesquisaMercadoria controle)
+        {
+            aguarde.Passo();
+            if (chkMetal.CheckedItems.Count > 0)
+                controle.FiltrarMetais((Metal[])(new ArrayList(chkMetal.CheckedItems).ToArray(typeof(Metal))));
+        }
+
+        private void FiltrarTipos(Aguarde aguarde, ControlePesquisaMercadoria controle)
+        {
+            aguarde.Passo();
+            if (chkTipo.CheckedItems.Count > 0)
+                controle.FiltrarTipos((MercadoriaTipo[])(new ArrayList(chkTipo.CheckedItems).ToArray(typeof(MercadoriaTipo))));
+        }
+
+        private void FiltrarValorMáximo(Aguarde aguarde, ControlePesquisaMercadoria controle)
+        {
+            aguarde.Passo();
+            if (txtValorMáximo.Double > 0 && txtCotação.Cotação != null)
+                controle.FiltrarÍndice(txtValorMáximo.Double / txtCotação.Cotação.Valor);
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
