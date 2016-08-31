a93d9cdf3e7aadd5a09b9e4622f42c3b55aaa373
André Pontes <andrepontes@gmail.com>
2016-08-31 14:20:46 -0300

    Adicionando dumps para comparação de componentes de custo

--- Integração/faz_dump.sql
+++ Integração/faz_dump.sql
@@ -5,3 +5,6 @@ mysql -u root $PWD $DB -e "select count(*) from mercadoria" >> t1.out
 mysql -u root $PWD $DB -e "select * from mercadoria" >> t2.out
 mysql -u root $PWD $DB -e  "select * from tabelamercadoria" >> t3.out
 mysql -u root $PWD $DB -e  "select count(*) from mercadoria where foradelinha=0" >> t4.out
+mysql -u root $PWD $DB -e  "select * from componente" >> t5.out
+mysql -u root $PWD $DB -e  "select * from componentecusto" >> t6.out
+mysql -u root $PWD $DB -e  "select * from vinculomercadoriacomponentecusto" >> t7.out
