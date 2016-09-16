a853b3750b2f275a152acfaab00d7e8281ddf2b1
André Pontes <andrepontes@gmail.com>
2016-09-16 14:14:00 -0300

    Mostrando mensagem no janela de seleção de pasta para importações fiscais

--- Programação/Indústria Mineira de Joias/Apresentação/Fiscal/BaseFiscal.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Fiscal/BaseFiscal.cs
@@ -6,19 +6,21 @@ using System.Windows.Forms;
 
 namespace Apresentação.Fiscal
 {
-    public partial class BaseFiscal : Formulários.BaseInferior
+    public partial class BaseFiscal : BaseInferior
     {
         public BaseFiscal()
         {
             InitializeComponent();
         }
 
-        private string ObterDiretório(string diretórioInicial)
+        private string ObterDiretório(string diretórioInicial, string mensagem)
         {
             FolderBrowserDialog janela = new FolderBrowserDialog();
 
             if (!String.IsNullOrEmpty(diretórioInicial))
                 janela.SelectedPath = diretórioInicial;
+
+            janela.Description = mensagem;
 
             if (janela.ShowDialog() != DialogResult.OK)
                 return null;
@@ -29,26 +31,29 @@ namespace Apresentação.Fiscal
         private void opçãoImportaçãoXMLAtacado_Click(object sender, EventArgs e)
         {
             ConfiguraçãoUsuário<string> diretórioInicial = new ConfiguraçãoUsuário<string>("diretórioInicialXmlAtacado", "");
-            string caminho = ObterDiretório(diretórioInicial);
-            diretórioInicial.Valor = caminho;
+            string caminho = ObterDiretório(diretórioInicial, "Selecione a pasta para importação de XML's de NF-e de atacado");
 
-            if (caminho != null)
-                new ImportadorXMLAtacado().ImportarXmls(caminho);
+            if (caminho == null)
+                return;
+
+            diretórioInicial.Valor = caminho;
+            new ImportadorXMLAtacado().ImportarXmls(caminho);
         }
 
         private void opçãoImportaçãoPDFAtacado_Click(object sender, EventArgs e)
         {
             ConfiguraçãoUsuário<string> diretórioInicial = new ConfiguraçãoUsuário<string>("diretórioInicialPdfAtacado", "");
-            string caminho = ObterDiretório(diretórioInicial);
+            string caminho = ObterDiretório(diretórioInicial, "Selecione a pasta para importação de PDF's das notas fiscais de atacado");
+
+            if (caminho == null)
+                return;
+
             diretórioInicial.Valor = caminho;
 
             string erros = null;
 
             AguardeDB.Mostrar();
-
-            if (caminho != null)
-                erros = new ImportadorPDFAtacado().ImportarPdfs(caminho);
-
+            erros = new ImportadorPDFAtacado().ImportarPdfs(caminho);
             AguardeDB.Fechar();
 
             if (!String.IsNullOrEmpty(erros))
