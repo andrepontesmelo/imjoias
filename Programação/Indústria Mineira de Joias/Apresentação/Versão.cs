41b90acb219f849700a22c4b865151df37ca0780
André Pontes <andrepontes@gmail.com>
2016-09-20 09:10:54 -0300

    Início da versão

--- Programação/Indústria Mineira de Joias/Apresentação/Versão.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Versão.cs
@@ -2,7 +2,7 @@
 {
     public class Versão
     {
-        public static string VersãoNumérica = "v0.6.9.0";
+        public static string VersãoNumérica = "v0.6.9.1";
         public static string NomeAplicação = "Indústria Mineira de Joias";
         public static string NomeVersãoAplicação = string.Format("{0} {1}", NomeAplicação, VersãoNumérica);
     }
