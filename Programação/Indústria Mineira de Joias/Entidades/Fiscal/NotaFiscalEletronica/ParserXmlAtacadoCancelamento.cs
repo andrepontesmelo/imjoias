fc8bb54fd1ab2cfe68beb76e93a9dc88c16e1702
André Pontes <andrepontes@gmail.com>
2016-09-30 14:22:10 -0300

    Interpretação de cancelamento de xml de atacado - red

---
+++ Programação/Indústria Mineira de Joias/Entidades/Fiscal/NotaFiscalEletronica/ParserXmlAtacadoCancelamento.cs
@@ -0,0 +1,16 @@
+﻿namespace Entidades.Fiscal.NotaFiscalEletronica
+{
+    public class ParserXmlAtacadoCancelamento
+    {
+        private string arquivo;
+
+        public ParserXmlAtacadoCancelamento(string arquivo)
+        {
+            this.arquivo = arquivo;
+        }
+
+        public bool Cancelamento => true;
+
+        public string Id { get; set; }
+    }
+}
---
+++ Programação/Indústria Mineira de Joias/EntidadesTests/Fiscal/NotaFiscalEletronica/ParserXmlAtacadoCancelamentoTests.cs
@@ -0,0 +1,41 @@
+﻿using Entidades.Fiscal.NotaFiscalEletronica;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+
+namespace EntidadesTests.Fiscal.NotaFiscalEletronica
+{
+    [TestClass()]
+    public class ParserXmlAtacadoCancelamentoTests
+    {
+        private static string ARQUIVO_NÃO_CANCELAMENTO = Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().Location)).FullName).FullName + @"\Arquivos\nfe.xml";
+
+        private static string ARQUIVO_CANCELAMENTO = Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().Location)).FullName).FullName + @"\Arquivos\cancelamento_nfe_saida.xml";
+
+        [TestMethod()]
+        public void DeveInterpretarNãoCancelamento()
+        {
+            ParserXmlAtacadoCancelamento parser = new ParserXmlAtacadoCancelamento(ARQUIVO_NÃO_CANCELAMENTO);
+            
+            Assert.IsFalse(parser.Cancelamento);
+        }
+
+        [TestMethod()]
+        public void DeveInterpretarCancelamento()
+        {
+            ParserXmlAtacadoCancelamento parser = new ParserXmlAtacadoCancelamento(ARQUIVO_CANCELAMENTO);
+
+            Assert.IsTrue(parser.Cancelamento);
+        }
+
+        [TestMethod()]
+        public void DeveInterpretarId()
+        {
+            ParserXmlAtacadoCancelamento parser = new ParserXmlAtacadoCancelamento(ARQUIVO_CANCELAMENTO);
+
+            Assert.AreEqual("ID31101218219329000103550010000000988538006600", parser.Id);
+        }
+    }
+}
