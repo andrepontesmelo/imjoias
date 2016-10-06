149b5a65b0a9bef44a8108ed6b1f68193faaa002
André Pontes <andrepontes@gmail.com>
2016-10-06 16:32:33 -0300

    Janela Importar - importação em lote

--- Programação/Indústria Mineira de Joias/Apresentação/Fiscal/JanelaImportação.Designer.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Fiscal/JanelaImportação.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnCancelar = new System.Windows.Forms.Button();
             this.btnIniciar = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtDiretório = new System.Windows.Forms.TextBox();
             this.chkEntradaXMLAtacado = new System.Windows.Forms.CheckBox();
             this.chkSaídaTDMVarejo = new System.Windows.Forms.CheckBox();
             this.chkSaídaPDFAtacado = new System.Windows.Forms.CheckBox();
@@ -70,6 +70,7 @@
             // btnIniciar
             // 
             this.btnIniciar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIniciar.Enabled = false;
             this.btnIniciar.Location = new System.Drawing.Point(222, 303);
             this.btnIniciar.Name = "btnIniciar";
             this.btnIniciar.Size = new System.Drawing.Size(75, 23);
@@ -78,16 +79,19 @@
             this.btnIniciar.UseVisualStyleBackColor = true;
             this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
             // 
-            // textBox1
+            // txtDiretório
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtDiretório.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(51, 112);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(327, 22);
-            this.textBox1.TabIndex = 5;
+            this.txtDiretório.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDiretório.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiretório.Location = new System.Drawing.Point(51, 112);
+            this.txtDiretório.Name = "txtDiretório";
+            this.txtDiretório.Size = new System.Drawing.Size(327, 22);
+            this.txtDiretório.TabIndex = 5;
+            this.txtDiretório.TextChanged += new System.EventHandler(this.txtDiretório_TextChanged);
+            this.txtDiretório.Validating += new System.ComponentModel.CancelEventHandler(this.txtDiretório_Validating);
+            this.txtDiretório.Validated += new System.EventHandler(this.controlesOpções_Validação);
             // 
             // chkEntradaXMLAtacado
             // 
@@ -98,6 +102,7 @@
             this.chkEntradaXMLAtacado.TabIndex = 6;
             this.chkEntradaXMLAtacado.Text = "XML @ Atacado";
             this.chkEntradaXMLAtacado.UseVisualStyleBackColor = true;
+            this.chkEntradaXMLAtacado.Click += new System.EventHandler(this.controlesOpções_Validação);
             // 
             // chkSaídaTDMVarejo
             // 
@@ -108,6 +113,7 @@
             this.chkSaídaTDMVarejo.TabIndex = 8;
             this.chkSaídaTDMVarejo.Text = "TDM @ Varejo";
             this.chkSaídaTDMVarejo.UseVisualStyleBackColor = true;
+            this.chkSaídaTDMVarejo.Click += new System.EventHandler(this.controlesOpções_Validação);
             // 
             // chkSaídaPDFAtacado
             // 
@@ -118,6 +124,7 @@
             this.chkSaídaPDFAtacado.TabIndex = 9;
             this.chkSaídaPDFAtacado.Text = "PDF @ Atacado";
             this.chkSaídaPDFAtacado.UseVisualStyleBackColor = true;
+            this.chkSaídaPDFAtacado.Click += new System.EventHandler(this.controlesOpções_Validação);
             // 
             // chkSaídaXMLAtacado
             // 
@@ -128,6 +135,7 @@
             this.chkSaídaXMLAtacado.TabIndex = 10;
             this.chkSaídaXMLAtacado.Text = "XML @ Atacado";
             this.chkSaídaXMLAtacado.UseVisualStyleBackColor = true;
+            this.chkSaídaXMLAtacado.Click += new System.EventHandler(this.controlesOpções_Validação);
             // 
             // label1
             // 
@@ -154,13 +162,15 @@
             this.btnPasta.BackgroundImage = global::Apresentação.Resource.folderopen1;
             this.btnPasta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnPasta.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnPasta.Location = new System.Drawing.Point(20, 110);
+            this.btnPasta.Location = new System.Drawing.Point(21, 110);
             this.btnPasta.Name = "btnPasta";
             this.btnPasta.Size = new System.Drawing.Size(24, 24);
             this.btnPasta.TabIndex = 13;
+            this.btnPasta.Click += new System.EventHandler(this.btnPasta_Click);
             // 
             // JanelaImportação
             // 
+            this.AcceptButton = this.btnIniciar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancelar;
@@ -172,14 +182,14 @@
             this.Controls.Add(this.chkSaídaPDFAtacado);
             this.Controls.Add(this.chkSaídaTDMVarejo);
             this.Controls.Add(this.chkEntradaXMLAtacado);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtDiretório);
             this.Controls.Add(this.btnIniciar);
             this.Controls.Add(this.btnCancelar);
             this.Name = "JanelaImportação";
             this.Text = "Importação";
             this.Controls.SetChildIndex(this.btnCancelar, 0);
             this.Controls.SetChildIndex(this.btnIniciar, 0);
-            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.txtDiretório, 0);
             this.Controls.SetChildIndex(this.chkEntradaXMLAtacado, 0);
             this.Controls.SetChildIndex(this.chkSaídaTDMVarejo, 0);
             this.Controls.SetChildIndex(this.chkSaídaPDFAtacado, 0);
@@ -197,7 +207,7 @@
 
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Button btnIniciar;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtDiretório;
         private System.Windows.Forms.CheckBox chkEntradaXMLAtacado;
         private System.Windows.Forms.CheckBox chkSaídaTDMVarejo;
         private System.Windows.Forms.CheckBox chkSaídaPDFAtacado;
--- Programação/Indústria Mineira de Joias/Apresentação/Fiscal/JanelaImportação.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Fiscal/JanelaImportação.cs
@@ -1,12 +1,42 @@
 ﻿using Apresentação.Formulários;
+using Entidades.Configuração;
+using Entidades.Fiscal.Importação;
+using Entidades.Fiscal.Importação.Resultado;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
 
 namespace Apresentação.Fiscal
 {
     public partial class JanelaImportação : JanelaExplicativa
     {
+        private const string MENSAGEM_ESCOLHA_DIRETÓRIO = "Escolha o diretório para a importação em lote.";
+
+        private Dictionary<BackgroundWorker, Aguarde> hashAguarde = new Dictionary<BackgroundWorker, Aguarde>();
+        ConfiguraçãoUsuário<string> diretórioInicial = new ConfiguraçãoUsuário<string>("diretórioInicialImportação", "");
+        ConfiguraçãoUsuário<bool> importarXMLAtacadoEntrada = new ConfiguraçãoUsuário<bool>("importarXMLAtacadoEntrada", true);
+        ConfiguraçãoUsuário<bool> importarXMLAtacadoSaída = new ConfiguraçãoUsuário<bool>("importarXMLAtacadoSaída", true);
+        ConfiguraçãoUsuário<bool> importarTDMVarejoSaída = new ConfiguraçãoUsuário<bool>("importarTDMVarejoSaída", true);
+        ConfiguraçãoUsuário<bool> importarPDFAtacadoSaída = new ConfiguraçãoUsuário<bool>("importarPDFAtacadoSaída", true);
+
         public JanelaImportação()
         {
             InitializeComponent();
+
+            CarregarOpções();
+        }
+
+        private void CarregarOpções()
+        {
+            chkEntradaXMLAtacado.Checked = importarXMLAtacadoEntrada.Valor;
+            chkSaídaXMLAtacado.Checked = importarXMLAtacadoSaída.Valor;
+            chkSaídaTDMVarejo.Checked = importarTDMVarejoSaída.Valor;
+            chkSaídaPDFAtacado.Checked = importarPDFAtacadoSaída.Valor;
+
+            txtDiretório.Text = diretórioInicial.Valor;
         }
 
         private void btnCancelar_Click(object sender, System.EventArgs e)
@@ -16,7 +46,172 @@ namespace Apresentação.Fiscal
 
         private void btnIniciar_Click(object sender, System.EventArgs e)
         {
-            Close();
+            PersistirOpções();
+            Importar();
+            Hide();
+        }
+
+        private void Importar()
+        {
+            if (chkSaídaXMLAtacado.Checked)
+                IniciarImportação(ImportadorSaídaXMLAtacado.DESCRIÇÃO, Thread_ImportarXMLAtacadoSaída);
+
+            if (chkSaídaPDFAtacado.Checked)
+                IniciarImportação(ImportadorSaídaPDFAtacado.DESCRIÇÃO, Thread_ImportarPDFAtacadoSaída);
+
+            if (chkSaídaTDMVarejo.Checked)
+                IniciarImportação(ImportadorSaídaTDMVarejo.DESCRIÇÃO, Thread_ImportarTDMVarejoSaída);
+
+            if (chkEntradaXMLAtacado.Checked)
+                IniciarImportação(ImportadorEntradaXMLAtacado.DESCRIÇÃO, Thread_ImportarXMLAtacadoEntrada);
+        }
+
+        private void PersistirOpções()
+        {
+            importarXMLAtacadoEntrada.Valor = chkEntradaXMLAtacado.Checked;
+            importarXMLAtacadoSaída.Valor = chkSaídaXMLAtacado.Checked;
+            importarTDMVarejoSaída.Valor = chkSaídaTDMVarejo.Checked;
+            importarPDFAtacadoSaída.Valor = chkSaídaPDFAtacado.Checked;
+        }
+
+
+        private string ObterDiretório(string diretórioInicial, string mensagem)
+        {
+            FolderBrowserDialog janela = new FolderBrowserDialog();
+
+            if (!String.IsNullOrEmpty(diretórioInicial))
+                janela.SelectedPath = diretórioInicial;
+
+            janela.Description = mensagem;
+
+            if (janela.ShowDialog() != DialogResult.OK)
+                return null;
+
+            return janela.SelectedPath;
+        }
+
+        private void IniciarImportação(string título, DoWorkEventHandler métodoExecução)
+        {
+            IniciarThreadJanelaAguarde(txtDiretório.Text, título, métodoExecução);
+        }
+
+        private void IniciarThreadJanelaAguarde(string caminho, string ação, DoWorkEventHandler métodoExecução)
+        {
+            Aguarde aguarde = CriarJanelaAguarde(ação);
+
+            BackgroundWorker thread = new BackgroundWorker();
+            thread.WorkerReportsProgress = true;
+            thread.ProgressChanged += Thread_ProgressChanged;
+            thread.RunWorkerCompleted += Thread_RunWorkerCompleted;
+            thread.DoWork += métodoExecução;
+            thread.RunWorkerAsync(caminho);
+
+            hashAguarde[thread] = aguarde;
+        }
+
+        private Aguarde CriarJanelaAguarde(string ação)
+        {
+            Aguarde janelaAguarde = new Aguarde(ação, 100);
+            janelaAguarde.TopMost = false;
+            janelaAguarde.Show(this);
+            return janelaAguarde;
+        }
+
+        private void Thread_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ObterJanelaAguarde((BackgroundWorker)sender).Close();
+
+            if (e.Error != null)
+            {
+                MessageBox.Show("Verifique se possui permissão de leitura na pasta.\n\n" + 
+                    e.Error.Message, "Erro na importação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            ResultadoImportação resultado = (ResultadoImportação)e.Result;
+            Process.Start("notepad", resultado.GravarArquivoTxt(Versão.NomeVersãoAplicação));
+        }
+
+        private Aguarde ObterJanelaAguarde(BackgroundWorker thread)
+        {
+            return hashAguarde[thread];
+        }
+
+        private void Thread_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Aguarde aguarde = ObterJanelaAguarde((BackgroundWorker)sender);
+            aguarde.Passos(e.ProgressPercentage, e.UserState as string);
+        }
+
+        private void Thread_ImportarXMLAtacadoSaída(object sender, DoWorkEventArgs e)
+        {
+            e.Result = new ImportadorSaídaXMLAtacado().ImportarArquivos(e.Argument as string, sender as BackgroundWorker);
+        }
+
+        private void Thread_ImportarCancelamentosAtacado(object sender, DoWorkEventArgs e)
+        {
+            e.Result = new ImportadorCancelamentosAtacado().ImportarArquivos(e.Argument as string, sender as BackgroundWorker);
+        }
+
+        private void Thread_ImportarXMLAtacadoEntrada(object sender, DoWorkEventArgs e)
+        {
+            e.Result = new ImportadorEntradaXMLAtacado().ImportarArquivos(e.Argument as string, sender as BackgroundWorker);
+        }
+
+        private void Thread_ImportarTDMVarejoSaída(object sender, DoWorkEventArgs e)
+        {
+            e.Result = new ImportadorSaídaTDMVarejo().ImportarArquivos(e.Argument as string, sender as BackgroundWorker);
+        }
+
+        private void Thread_ImportarPDFAtacadoSaída(object sender, DoWorkEventArgs e)
+        {
+            e.Result = new ImportadorSaídaPDFAtacado().ImportarArquivos(e.Argument as string, sender as BackgroundWorker);
+        }
+
+        private void btnPasta_Click(object sender, EventArgs e)
+        {
+            string diretório = ObterDiretório(diretórioInicial.Valor, MENSAGEM_ESCOLHA_DIRETÓRIO);
+
+            if (diretório == null)
+                return;
+
+            txtDiretório.Text = diretório;
+            diretórioInicial.Valor = diretório;
+        }
+
+        private void txtDiretório_Validating(object sender, CancelEventArgs e)
+        {
+            e.Cancel = !DiretórioVazio && !DiretórioVálido;
+        }
+
+        private bool DiretórioVálido => Directory.Exists(txtDiretório.Text);
+        private bool DiretórioVazio => String.IsNullOrWhiteSpace(txtDiretório.Text);
+
+        public bool AlgumaOpçãoSelecionada
+        {
+            get
+            {
+                return chkEntradaXMLAtacado.Checked ||
+                    chkSaídaPDFAtacado.Checked ||
+                    chkSaídaTDMVarejo.Checked ||
+                    chkSaídaXMLAtacado.Checked;
+            }
+        }
+
+        private void controlesOpções_Validação(object sender, EventArgs e)
+        {
+            AtualizarBotãoIniciar();
+        }
+
+        private void AtualizarBotãoIniciar()
+        {
+            btnIniciar.Enabled = DiretórioVálido && AlgumaOpçãoSelecionada;
+        }
+
+        private void txtDiretório_TextChanged(object sender, EventArgs e)
+        {
+            AtualizarBotãoIniciar();
         }
     }
 }
