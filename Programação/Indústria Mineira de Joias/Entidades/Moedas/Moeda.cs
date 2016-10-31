dbfefde817e46e2faa85d474f114df338467c7a6
André Pontes Melo <andrepontes@gmail.com>
2016-10-31 15:59:19 -0200

    Otimização da carga de moedas.

--- Programação/Indústria Mineira de Joias/Entidades/Moedas/Moeda.cs
+++ Programação/Indústria Mineira de Joias/Entidades/Moedas/Moeda.cs
@@ -1,11 +1,13 @@
 ﻿using Acesso.Comum;
 using Acesso.Comum.Cache;
 using Entidades.Mercadoria.Componente;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace Entidades.Moedas
 {
-    [DbTransação, Cacheável("ObterMoeda"), DbTabela("moeda"), Validade(6, 0, 0), NãoCopiarCache]
+    [DbTransação, Cacheável("ObterMoeda"), DbTabela("moeda"), NãoCopiarCache]
     public class Moeda : DbManipulaçãoAutomática
     {
 #pragma warning disable 0649            // Field 'field' is never assigned to, and will always have its default value 'value'
@@ -26,10 +28,11 @@ namespace Entidades.Moedas
 #pragma warning restore 0649            // Field 'field' is never assigned to, and will always have its default value 'value'
 
         public uint Código => código;
-
         public bool Sistema  => sistema;
 
         public string Nome { get { return nome; } set { nome = value; DefinirDesatualizado(); } }
+        public Image Ícone { get { return ícone; } set { ícone = value; DefinirDesatualizado(); } }
+        public byte CasasDecimais { get { return casasDecimais; } set { casasDecimais = value; DefinirDesatualizado(); } }
 
         public ComponenteCusto ComponenteCusto
         {
@@ -37,25 +40,24 @@ namespace Entidades.Moedas
             set { componenteCusto = value; DefinirDesatualizado(); }
         }
 
-        public Image Ícone { get { return ícone; } set { ícone = value; DefinirDesatualizado(); } }
-        public byte CasasDecimais { get { return casasDecimais; } set { casasDecimais = value; DefinirDesatualizado(); } }
+        private static List<Moeda> moedas = null;
 
         public static Moeda ObterMoeda(uint código)
         {
-            return MapearÚnicaLinha<Moeda>(
-                "SELECT * FROM moeda WHERE codigo = " + DbTransformar(código));
-
+            return (from moeda in ObterMoedas() where moeda.Código.Equals(código) select moeda).First();
         }
 
-        public static Moeda[] ObterMoedas()
+        public static List<Moeda> ObterMoedas()
         {
-            return Mapear<Moeda>(
-                "SELECT * FROM moeda ORDER BY nome").ToArray();
+            if (moedas == null)
+                moedas = Mapear<Moeda>("SELECT * FROM moeda ORDER BY nome");
+
+            return moedas;
         }
 
         public static Moeda ObterMoeda(MoedaSistema moeda)
         {
-            return ObterMoeda((uint)moeda);
+            return ObterMoeda((uint) moeda);
         }
     }
 }
