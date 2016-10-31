e58185be9b786fde0f82ec597739812544f17742
André Pontes Melo <andrepontes@gmail.com>
2016-10-31 08:28:15 -0200

    Limpeza do código

--- Programação/Indústria Mineira de Joias/Apresentação/Formulário/Lista/ListViewColumnSorter.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Formulário/Lista/ListViewColumnSorter.cs
@@ -7,9 +7,6 @@
 
 namespace Apresentação.Formulários
 {
-    /// <summary>
-    /// This class is an implementation of the 'IComparer' interface.
-    /// </summary>
     public class ListViewColumnSorter : IComparer
     {
         public enum SortModifiers
@@ -19,18 +16,8 @@ namespace Apresentação.Formulários
             SortByText
         }
 
-        /// <summary>
-        /// Specifies the column to be sorted
-        /// </summary>
         public int ColumnToSort;
-        /// <summary>
-        /// Specifies the order in which to sort (i.e. 'Ascending').
-        /// </summary>
         public SortOrder OrderOfSort;
-        /// <summary>
-        /// Case insensitive comparer object
-        /// </summary>
-
         private NumberCaseInsensitiveComparer ObjectCompare;
         private ImageTextComparer FirstObjectCompare;
         private CheckboxTextComparer FirstObjectCompare2;
@@ -48,49 +35,33 @@ namespace Apresentação.Formulários
             }
         }
 
-        /// <summary>
-        /// Class constructor.  Initializes various elements
-        /// </summary>
         public ListViewColumnSorter()
         {
-            // Initialize the column to '0'
             ColumnToSort = 0;
 
-            // Initialize the CaseInsensitiveComparer object
             ObjectCompare = new NumberCaseInsensitiveComparer();
             FirstObjectCompare = new ImageTextComparer();
             FirstObjectCompare2 = new CheckboxTextComparer();
         }
 
-        /// <summary>
-        /// This method is inherited from the IComparer interface.  It compares the two objects passed using a case insensitive comparison.
-        /// </summary>
-        /// <param name="x">First object to be compared</param>
-        /// <param name="y">Second object to be compared</param>
-        /// <returns>The result of the comparison. "0" if equal, negative if 'x' is less than 'y' and positive if 'x' is greater than 'y'</returns>
         public int Compare(object x, object y)
         {
             int compareResult = 0;
             ListViewItem listviewX, listviewY;
 
-            // Cast the objects to be compared to ListViewItem objects
             listviewX = (ListViewItem)x;
             listviewY = (ListViewItem)y;
 
             ListView listViewMain = listviewX.ListView;
 
-            // Calculate correct return value based on object comparison
             if (listViewMain.Sorting != SortOrder.Ascending &&
                 listViewMain.Sorting != SortOrder.Descending)
             {
-                // Return '0' to indicate they are equal
                 return compareResult;
             }
 
             if (mySortModifier.Equals(SortModifiers.SortByText) || ColumnToSort > 0)
             {
-                // Compare the two items
-
                 if (listviewX.SubItems.Count <= ColumnToSort &&
                     listviewY.SubItems.Count <= ColumnToSort)
                 {
@@ -126,27 +97,20 @@ namespace Apresentação.Formulários
                 }
             }
 
-            // Calculate correct return value based on object comparison
             if (OrderOfSort == SortOrder.Ascending)
             {
-                // Ascending sort is selected, return normal result of compare operation
                 return compareResult;
             }
             else if (OrderOfSort == SortOrder.Descending)
             {
-                // Descending sort is selected, return negative result of compare operation
                 return (-compareResult);
             }
             else
             {
-                // Return '0' to indicate they are equal
                 return 0;
             }
         }
 
-        /// <summary>
-        /// Gets or sets the number of the column to which to apply the sorting operation (Defaults to '0').
-        /// </summary>
         public int SortColumn
         {
             set
@@ -159,9 +123,6 @@ namespace Apresentação.Formulários
             }
         }
 
-        /// <summary>
-        /// Gets or sets the order of sorting to apply (for example, 'Ascending' or 'Descending').
-        /// </summary>
         public SortOrder Order
         {
             set
@@ -180,10 +141,8 @@ namespace Apresentação.Formulários
             if (listView.Sorting == SortOrder.None)
                 listView.Sorting = SortOrder.Ascending;
 
-            // Determine if clicked column is already the column that is being sorted.
             if (e.Column == SortColumn)
             {
-                // Reverse the current sort direction for this column.
                 if (Order == SortOrder.Ascending)
                 {
                     Order = SortOrder.Descending;
@@ -195,34 +154,28 @@ namespace Apresentação.Formulários
             }
             else
             {
-                // Set the column number that is to be sorted; default to ascending.
                 SortColumn = e.Column;
                 Order = SortOrder.Ascending;
             }
 
-            // Perform the sort with these new sort options.
             listView.Sort();
         }
     }
 
     public class ImageTextComparer : IComparer
     {
-        //private CaseInsensitiveComparer ObjectCompare;
         private NumberCaseInsensitiveComparer ObjectCompare;
 
         public ImageTextComparer()
         {
-            // Initialize the CaseInsensitiveComparer object
             ObjectCompare = new NumberCaseInsensitiveComparer();
         }
 
         public int Compare(object x, object y)
         {
-            //int compareResult;
             int image1, image2;
             ListViewItem listviewX, listviewY;
 
-            // Cast the objects to be compared to ListViewItem objects
             listviewX = (ListViewItem)x;
             image1 = listviewX.ImageIndex;
             listviewY = (ListViewItem)y;
@@ -249,13 +202,11 @@ namespace Apresentação.Formulários
 
         public CheckboxTextComparer()
         {
-            // Initialize the CaseInsensitiveComparer object
             ObjectCompare = new NumberCaseInsensitiveComparer();
         }
 
         public int Compare(object x, object y)
         {
-            // Cast the objects to be compared to ListViewItem objects
             ListViewItem listviewX = (ListViewItem)x;
             ListViewItem listviewY = (ListViewItem)y;
 
@@ -354,26 +305,13 @@ namespace Apresentação.Formulários
             }
         }
 
-        // deprecated
-        //private bool IsWholeNumber(string strNumber)
-        //{
-        //    Regex wholePattern = new Regex(@"^\d+$");
-        //    return wholePattern.IsMatch(strNumber);
-        //}
-
         private string GetNumberDecimalSeparator()
         {
-            return System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            return CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
         }
 
-        // http://stackoverflow.com/questions/4246077/matching-numbers-with-regular-expressions-only-digits-and-commas/4247184#4247184
-        // https://www.debuggex.com/r/Lyx0F0y1LORvNhwA
         private bool IsDecimalNumber(string strNumber)
         {
-            //@"^-?(\d+|(\d{1,3}((,|\.)\d{3})*))((,|\.)\d+)?$"
-
-            //string regex = @"^-?(?:(?:0|[1-9][0-9]*)(?:" + GetNumberDecimalSeparator() + @"[0-9]+)?|[1-9][0-9]{1,2}(?:,[0-9]{3})+)$";
-
             string regex = @"^-?(\d+|(\d{1,3}((,|\.)\d{3})*))((,|\.)\d+)?$";
 
             Regex wholePattern = new Regex(regex);
