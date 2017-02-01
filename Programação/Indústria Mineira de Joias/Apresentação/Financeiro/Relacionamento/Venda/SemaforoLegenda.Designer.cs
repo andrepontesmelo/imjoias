cec6e0071c3bdcce9ec12179bc47b8d4658c1bb4
André Pontes <andrepontes@gmail.com>
2017-02-01 16:34:49 -0200

    Semáforo com checkboxes.

--- Programação/Indústria Mineira de Joias/Apresentação/Financeiro/Relacionamento/Venda/SemaforoLegenda.Designer.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Financeiro/Relacionamento/Venda/SemaforoLegenda.Designer.cs
@@ -35,6 +35,12 @@
             this.opçãoComissãoFechada = new Apresentação.Formulários.Opção();
             this.opçãoNãoQuitada = new Apresentação.Formulários.Opção();
             this.opçãoCobrança = new Apresentação.Formulários.Opção();
+            this.chkVendaDia = new System.Windows.Forms.CheckBox();
+            this.chkNFe = new System.Windows.Forms.CheckBox();
+            this.chkCobrança = new System.Windows.Forms.CheckBox();
+            this.chkNãoQuitada = new System.Windows.Forms.CheckBox();
+            this.chkQuitadaAberta = new System.Windows.Forms.CheckBox();
+            this.chkComissãoFechada = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // opçãoVendaDia
@@ -42,7 +48,7 @@
             this.opçãoVendaDia.BackColor = System.Drawing.Color.Transparent;
             this.opçãoVendaDia.Descrição = "Venda do dia";
             this.opçãoVendaDia.Imagem = ((System.Drawing.Image)(resources.GetObject("opçãoVendaDia.Imagem")));
-            this.opçãoVendaDia.Location = new System.Drawing.Point(5, 30);
+            this.opçãoVendaDia.Location = new System.Drawing.Point(24, 31);
             this.opçãoVendaDia.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.opçãoVendaDia.MaximumSize = new System.Drawing.Size(150, 100);
             this.opçãoVendaDia.MinimumSize = new System.Drawing.Size(150, 16);
@@ -56,7 +62,7 @@
             this.opçãoNFe.BackColor = System.Drawing.Color.Transparent;
             this.opçãoNFe.Descrição = "Nota fiscal";
             this.opçãoNFe.Imagem = ((System.Drawing.Image)(resources.GetObject("opçãoNFe.Imagem")));
-            this.opçãoNFe.Location = new System.Drawing.Point(5, 50);
+            this.opçãoNFe.Location = new System.Drawing.Point(24, 51);
             this.opçãoNFe.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.opçãoNFe.MaximumSize = new System.Drawing.Size(150, 100);
             this.opçãoNFe.MinimumSize = new System.Drawing.Size(150, 16);
@@ -70,7 +76,7 @@
             this.opçãoQuitadaAberta.BackColor = System.Drawing.Color.Transparent;
             this.opçãoQuitadaAberta.Descrição = "Quitada; c. aberta";
             this.opçãoQuitadaAberta.Imagem = ((System.Drawing.Image)(resources.GetObject("opçãoQuitadaAberta.Imagem")));
-            this.opçãoQuitadaAberta.Location = new System.Drawing.Point(5, 110);
+            this.opçãoQuitadaAberta.Location = new System.Drawing.Point(24, 111);
             this.opçãoQuitadaAberta.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.opçãoQuitadaAberta.MaximumSize = new System.Drawing.Size(150, 100);
             this.opçãoQuitadaAberta.MinimumSize = new System.Drawing.Size(150, 16);
@@ -84,7 +90,7 @@
             this.opçãoComissãoFechada.BackColor = System.Drawing.Color.Transparent;
             this.opçãoComissãoFechada.Descrição = "Comissão fechada";
             this.opçãoComissãoFechada.Imagem = ((System.Drawing.Image)(resources.GetObject("opçãoComissãoFechada.Imagem")));
-            this.opçãoComissãoFechada.Location = new System.Drawing.Point(5, 130);
+            this.opçãoComissãoFechada.Location = new System.Drawing.Point(24, 131);
             this.opçãoComissãoFechada.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.opçãoComissãoFechada.MaximumSize = new System.Drawing.Size(150, 100);
             this.opçãoComissãoFechada.MinimumSize = new System.Drawing.Size(150, 16);
@@ -98,7 +104,7 @@
             this.opçãoNãoQuitada.BackColor = System.Drawing.Color.Transparent;
             this.opçãoNãoQuitada.Descrição = "Não quitada";
             this.opçãoNãoQuitada.Imagem = ((System.Drawing.Image)(resources.GetObject("opçãoNãoQuitada.Imagem")));
-            this.opçãoNãoQuitada.Location = new System.Drawing.Point(5, 90);
+            this.opçãoNãoQuitada.Location = new System.Drawing.Point(24, 91);
             this.opçãoNãoQuitada.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.opçãoNãoQuitada.MaximumSize = new System.Drawing.Size(150, 100);
             this.opçãoNãoQuitada.MinimumSize = new System.Drawing.Size(150, 16);
@@ -112,7 +118,7 @@
             this.opçãoCobrança.BackColor = System.Drawing.Color.Transparent;
             this.opçãoCobrança.Descrição = "Cobrança";
             this.opçãoCobrança.Imagem = ((System.Drawing.Image)(resources.GetObject("opçãoCobrança.Imagem")));
-            this.opçãoCobrança.Location = new System.Drawing.Point(5, 70);
+            this.opçãoCobrança.Location = new System.Drawing.Point(24, 71);
             this.opçãoCobrança.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.opçãoCobrança.MaximumSize = new System.Drawing.Size(150, 100);
             this.opçãoCobrança.MinimumSize = new System.Drawing.Size(150, 16);
@@ -121,10 +127,76 @@
             this.opçãoCobrança.TabIndex = 7;
             this.opçãoCobrança.Click += new System.EventHandler(this.opçãoCobrança_Click);
             // 
+            // chkVendaDia
+            // 
+            this.chkVendaDia.BackColor = System.Drawing.Color.Transparent;
+            this.chkVendaDia.Location = new System.Drawing.Point(7, 30);
+            this.chkVendaDia.Name = "chkVendaDia";
+            this.chkVendaDia.Size = new System.Drawing.Size(16, 20);
+            this.chkVendaDia.TabIndex = 8;
+            this.chkVendaDia.UseVisualStyleBackColor = false;
+            this.chkVendaDia.CheckedChanged += new System.EventHandler(this.chkVendaDia_CheckedChanged);
+            // 
+            // chkNFe
+            // 
+            this.chkNFe.BackColor = System.Drawing.Color.Transparent;
+            this.chkNFe.Location = new System.Drawing.Point(7, 50);
+            this.chkNFe.Name = "chkNFe";
+            this.chkNFe.Size = new System.Drawing.Size(16, 20);
+            this.chkNFe.TabIndex = 9;
+            this.chkNFe.UseVisualStyleBackColor = false;
+            this.chkNFe.CheckedChanged += new System.EventHandler(this.chkNFe_CheckedChanged);
+            // 
+            // chkCobrança
+            // 
+            this.chkCobrança.BackColor = System.Drawing.Color.Transparent;
+            this.chkCobrança.Location = new System.Drawing.Point(7, 70);
+            this.chkCobrança.Name = "chkCobrança";
+            this.chkCobrança.Size = new System.Drawing.Size(16, 20);
+            this.chkCobrança.TabIndex = 10;
+            this.chkCobrança.UseVisualStyleBackColor = false;
+            this.chkCobrança.CheckedChanged += new System.EventHandler(this.chkCobrança_CheckedChanged);
+            // 
+            // chkNãoQuitada
+            // 
+            this.chkNãoQuitada.BackColor = System.Drawing.Color.Transparent;
+            this.chkNãoQuitada.Location = new System.Drawing.Point(7, 90);
+            this.chkNãoQuitada.Name = "chkNãoQuitada";
+            this.chkNãoQuitada.Size = new System.Drawing.Size(16, 20);
+            this.chkNãoQuitada.TabIndex = 11;
+            this.chkNãoQuitada.UseVisualStyleBackColor = false;
+            this.chkNãoQuitada.CheckedChanged += new System.EventHandler(this.chkNãoQuitada_CheckedChanged);
+            // 
+            // chkQuitadaAberta
+            // 
+            this.chkQuitadaAberta.BackColor = System.Drawing.Color.Transparent;
+            this.chkQuitadaAberta.Location = new System.Drawing.Point(7, 110);
+            this.chkQuitadaAberta.Name = "chkQuitadaAberta";
+            this.chkQuitadaAberta.Size = new System.Drawing.Size(16, 20);
+            this.chkQuitadaAberta.TabIndex = 12;
+            this.chkQuitadaAberta.UseVisualStyleBackColor = false;
+            this.chkQuitadaAberta.CheckedChanged += new System.EventHandler(this.chkQuitadaAberta_CheckedChanged);
+            // 
+            // chkComissãoFechada
+            // 
+            this.chkComissãoFechada.BackColor = System.Drawing.Color.Transparent;
+            this.chkComissãoFechada.Location = new System.Drawing.Point(7, 130);
+            this.chkComissãoFechada.Name = "chkComissãoFechada";
+            this.chkComissãoFechada.Size = new System.Drawing.Size(16, 20);
+            this.chkComissãoFechada.TabIndex = 13;
+            this.chkComissãoFechada.UseVisualStyleBackColor = false;
+            this.chkComissãoFechada.CheckedChanged += new System.EventHandler(this.chkComissãoFechada_CheckedChanged);
+            // 
             // SemaforoLegenda
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chkComissãoFechada);
+            this.Controls.Add(this.chkQuitadaAberta);
+            this.Controls.Add(this.chkNãoQuitada);
+            this.Controls.Add(this.chkCobrança);
+            this.Controls.Add(this.chkNFe);
+            this.Controls.Add(this.chkVendaDia);
             this.Controls.Add(this.opçãoCobrança);
             this.Controls.Add(this.opçãoNãoQuitada);
             this.Controls.Add(this.opçãoComissãoFechada);
@@ -132,7 +204,7 @@
             this.Controls.Add(this.opçãoNFe);
             this.Controls.Add(this.opçãoVendaDia);
             this.Name = "SemaforoLegenda";
-            this.Size = new System.Drawing.Size(133, 155);
+            this.Size = new System.Drawing.Size(176, 155);
             this.Título = "Legenda";
             this.Controls.SetChildIndex(this.opçãoVendaDia, 0);
             this.Controls.SetChildIndex(this.opçãoNFe, 0);
@@ -140,6 +212,12 @@
             this.Controls.SetChildIndex(this.opçãoComissãoFechada, 0);
             this.Controls.SetChildIndex(this.opçãoNãoQuitada, 0);
             this.Controls.SetChildIndex(this.opçãoCobrança, 0);
+            this.Controls.SetChildIndex(this.chkVendaDia, 0);
+            this.Controls.SetChildIndex(this.chkNFe, 0);
+            this.Controls.SetChildIndex(this.chkCobrança, 0);
+            this.Controls.SetChildIndex(this.chkNãoQuitada, 0);
+            this.Controls.SetChildIndex(this.chkQuitadaAberta, 0);
+            this.Controls.SetChildIndex(this.chkComissãoFechada, 0);
             this.ResumeLayout(false);
 
         }
@@ -152,5 +230,11 @@
         private Formulários.Opção opçãoComissãoFechada;
         private Formulários.Opção opçãoNãoQuitada;
         private Formulários.Opção opçãoCobrança;
+        private System.Windows.Forms.CheckBox chkVendaDia;
+        private System.Windows.Forms.CheckBox chkNFe;
+        private System.Windows.Forms.CheckBox chkCobrança;
+        private System.Windows.Forms.CheckBox chkNãoQuitada;
+        private System.Windows.Forms.CheckBox chkQuitadaAberta;
+        private System.Windows.Forms.CheckBox chkComissãoFechada;
     }
 }
--- Programação/Indústria Mineira de Joias/Apresentação/Financeiro/Relacionamento/Venda/SemaforoLegenda.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Financeiro/Relacionamento/Venda/SemaforoLegenda.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Apresentação.Formulários;
 using Entidades.Relacionamento.Venda;
+using Entidades.Configuração;
+using System.Windows.Forms;
 
 namespace Apresentação.Financeiro.Venda
 {
@@ -11,11 +13,41 @@ namespace Apresentação.Financeiro.Venda
         private Opção[] opções;
         private string[] descriçãoLegendas;
 
+        private ConfiguraçãoUsuário<bool> mostrarNãoQuitada;
+        private ConfiguraçãoUsuário<bool> mostrarCobrança;
+        private ConfiguraçãoUsuário<bool> mostrarQuitadaAberta;
+        private ConfiguraçãoUsuário<bool> mostrarComissãoFechada;
+        private ConfiguraçãoUsuário<bool> mostrarVendaDia;
+        private ConfiguraçãoUsuário<bool> mostrarNFe;
+
         public SemaforoLegenda()
         {
             InitializeComponent();
             opções = new Opção[6] { opçãoNãoQuitada, opçãoCobrança, opçãoQuitadaAberta, opçãoComissãoFechada, opçãoVendaDia, opçãoNFe };
             descriçãoLegendas = new string[6] { "Não quitada", "Cobrança", "Quitada; c. aberta", "Comissão fechada", "Venda do dia", "Nota fiscal" };
+
+            CarregarConfigurações();
+            CarregarMarcações();
+        }
+
+        private void CarregarConfigurações()
+        {
+            mostrarNãoQuitada = new ConfiguraçãoUsuário<bool>("semáforo_mostrar_não_quitada", true);
+            mostrarCobrança = new ConfiguraçãoUsuário<bool>("semáforo_mostrar_cobrança", true);
+            mostrarQuitadaAberta = new ConfiguraçãoUsuário<bool>("semáforo_mostrar_quitada_aberta", true);
+            mostrarComissãoFechada = new ConfiguraçãoUsuário<bool>("semáforo_mostrar_comissão_fechada", true);
+            mostrarVendaDia = new ConfiguraçãoUsuário<bool>("semáforo_mostrar_venda_dia", true);
+            mostrarNFe = new ConfiguraçãoUsuário<bool>("semáforo_mostrar_nfe", true);
+        }
+
+        private void CarregarMarcações()
+        {
+            CarregarMarcação(mostrarNãoQuitada, chkNãoQuitada);
+            CarregarMarcação(mostrarCobrança, chkCobrança);
+            CarregarMarcação(mostrarQuitadaAberta, chkQuitadaAberta);
+            CarregarMarcação(mostrarComissãoFechada, chkComissãoFechada);
+            CarregarMarcação(mostrarVendaDia, chkVendaDia);
+            CarregarMarcação(mostrarNFe, chkNFe);
         }
 
         private void opçãoVendaDia_Click(object sender, EventArgs e)
@@ -58,6 +90,46 @@ namespace Apresentação.Financeiro.Venda
                 opções[x].Descrição = descriçãoLegendas[x] + 
                     (possuiVenda ?  string.Format(" ({0})", legendas[x], descriçãoLegendas[x]) : "");
             }
+        }
+
+        private void PersistirMarcação(ConfiguraçãoUsuário<bool> configuração, CheckBox controle)
+        {
+            configuração.Valor = controle.Checked;
+        }
+
+        private void CarregarMarcação(ConfiguraçãoUsuário<bool> configuração, CheckBox controle)
+        {
+            controle.Checked = configuração.Valor;
+        }
+
+        private void chkVendaDia_CheckedChanged(object sender, EventArgs e)
+        {
+            PersistirMarcação(mostrarVendaDia, chkVendaDia);
+        }
+
+        private void chkNFe_CheckedChanged(object sender, EventArgs e)
+        {
+            PersistirMarcação(mostrarNFe, chkNFe);
+        }
+
+        private void chkCobrança_CheckedChanged(object sender, EventArgs e)
+        {
+            PersistirMarcação(mostrarCobrança, chkCobrança);
+        }
+
+        private void chkNãoQuitada_CheckedChanged(object sender, EventArgs e)
+        {
+            PersistirMarcação(mostrarNãoQuitada, chkNãoQuitada);
+        }
+
+        private void chkQuitadaAberta_CheckedChanged(object sender, EventArgs e)
+        {
+            PersistirMarcação(mostrarQuitadaAberta, chkQuitadaAberta);
+        }
+
+        private void chkComissãoFechada_CheckedChanged(object sender, EventArgs e)
+        {
+            PersistirMarcação(mostrarComissãoFechada, chkComissãoFechada);
         }
     }
 }
