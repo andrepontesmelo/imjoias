38fe9033e078dd5589d088426f1c11bfd90c1329
André Pontes <andrepontes@gmail.com>
2017-02-10 09:32:35 -0200

    Atualização da versão

--- Programação/Indústria Mineira de Joias/Apresentação/Versão.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Versão.cs
@@ -2,9 +2,9 @@
 {
     public class Versão
     {
-        public static string GithubBranch = "v0.7.0.0";
+        public static string GithubBranch = "v0.7.0.1";
         public static string NomeAplicação = "Indústria Mineira de Joias";
-        public static bool Beta = true;
+        public static bool Beta = false;
 
         public static string Descrição = string.Format("{0} {1} {2}", 
             NomeAplicação, 
