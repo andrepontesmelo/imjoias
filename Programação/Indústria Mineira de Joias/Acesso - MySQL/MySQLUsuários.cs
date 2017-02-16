e855de5f47dc9f1d0f4a3753b1887cd2cf424eb7
André Pontes <andrepontes@gmail.com>
2017-02-16 16:23:53 -0200

    Atualização do ip do banco de dados de desenvolvimento

--- Programação/Indústria Mineira de Joias/Acesso - MySQL/MySQLUsuários.cs
+++ Programação/Indústria Mineira de Joias/Acesso - MySQL/MySQLUsuários.cs
@@ -17,8 +17,7 @@ namespace Acesso.MySQL
         private const string bdPadrão = "imjoias";
 
 #if DEBUG
-        //private const string hostPadrão = "192.168.144.1";
-        private const string hostPadrão = "192.168.1.110";
+        private const string hostPadrão = "192.168.1.45";
         private const int portPadrão = 3306;
 #else
         private const string hostPadrão = "192.168.1.25";
