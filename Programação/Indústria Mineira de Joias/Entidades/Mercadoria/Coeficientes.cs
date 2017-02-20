4f8ac05ce819ec269575c34a9d87aaab40cfab13
André Pontes <andrepontes@gmail.com>
2017-02-20 16:38:34 -0300

    Redução do uso de memória: uso de ListDictionary ao invés de Hash.
    Era utilizado uma Hash para indicar os coeficientes de uma referência, ou seja, era criada uma hash para cada referência. Com a modificação, é criada uma lista encadiada para armazenamento dos índices.

--- Programação/Indústria Mineira de Joias/Entidades/Mercadoria/Coeficientes.cs
+++ Programação/Indústria Mineira de Joias/Entidades/Mercadoria/Coeficientes.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections.Specialized;
 
 namespace Entidades.Mercadoria
 {
@@ -9,16 +8,18 @@ namespace Entidades.Mercadoria
     /// </summary>
     public class Coeficientes
     {
-        private Dictionary<uint, double> hashTabela;
+        // Chave: uint. Código da tabela.
+        // Valor: double. Coeficiente da mercadoria naquela tabela.
+        private ListDictionary hashTabela;
 
         public Coeficientes()
         {
-            hashTabela = new Dictionary<uint, double>();
+            hashTabela = new ListDictionary();
         }
 
         internal void AdicionarCoeficiente(uint tabela, double valor)
         {
-            if (hashTabela.ContainsKey(tabela))
+            if (hashTabela.Contains(tabela))
             {
                 throw new Exception("Tentativa de adicionar um indice ja cadastrado, para tabela: " + tabela.ToString() + " valor já existente: " + hashTabela[tabela].ToString() + " valor a ser colocado: " + valor.ToString());
             }
@@ -34,9 +35,9 @@ namespace Entidades.Mercadoria
                     throw new ArgumentNullException("Tabela não pode ser nula!");
 
                 if (tabela.Código == 2 || tabela.Código == 6 || tabela.Código == 7)
-                    return hashTabela[3];
+                    return (double) hashTabela[(uint) 3];
                 else
-                    return hashTabela[tabela.Código];
+                    return (double) hashTabela[tabela.Código];
             }
         }
     }
