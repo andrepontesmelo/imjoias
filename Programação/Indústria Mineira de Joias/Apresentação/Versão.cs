f9d74c19f5c91af65c0ca14c945273bf559955d3
André Pontes <andrepontes@gmail.com>
2017-03-17 16:17:41 -0300

    Atualizando compilador do C# via pacote Nuget

--- Programação/Indústria Mineira de Joias/Apresentação/Versão.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Versão.cs
@@ -4,11 +4,11 @@
     {
         public static string GithubBranch = "v0.7.0.1";
         public static string NomeAplicação = "Indústria Mineira de Joias";
-        public static bool Beta = false;
+        public static bool Beta = true;
 
         public static string Descrição = string.Format("{0} {1} {2}", 
             NomeAplicação, 
             GithubBranch,
-            Beta ? "Beta" : "");
+            Beta ? "Beta - 17 de março 14h" : "");
     }
 }
