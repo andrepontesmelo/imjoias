e7c556288e9befdefe1bfb4ee02277ff30857755
André Pontes <andrepontes@gmail.com>
2017-03-24 15:12:29 -0300

    Listagem de pedidos têm rastreamento com 8 dígitos

--- Programação/Indústria Mineira de Joias/Entidades/Mercadoria/Mercadoria.cs
+++ Programação/Indústria Mineira de Joias/Entidades/Mercadoria/Mercadoria.cs
@@ -1104,35 +1104,37 @@ namespace Entidades.Mercadoria
             {
                 using (IDbCommand cmd = conexão.CreateCommand())
                 {
-                    cmd.CommandTimeout = 120;
                     
-                    cmd.CommandText = string.Format("SELECT p.codigo, p.nome, a.previsao, SUM(si.quantidade) - ifnull(rastreamentovenda.qtd,0) - ifnull(rastreamentoretorno.qtd,0) as saldo, si.referencia " +
-                         " FROM saida s JOIN saidaitem si ON s.codigo = si.saida JOIN acertoconsignado a ON a.codigo = s.acerto " +
-                         " JOIN pessoa p ON p.codigo = a.cliente " +
-                         " left join " +
-                         " ( SELECT p.codigo, SUM(vi.quantidade) as qtd, referencia " +
-                         " FROM venda v JOIN vendaitem vi ON v.codigo = vi.venda JOIN acertoconsignado a ON a.codigo = v.acerto " +
-                         " JOIN pessoa p ON p.codigo = a.cliente " +
-                         " WHERE a.dataEfetiva IS NULL " +
-                         " GROUP BY p.codigo, vi.referencia " +
-                         " ) " +
-                         " rastreamentovenda on p.codigo=rastreamentovenda.codigo " +
-                         " and substr(si.referencia,1,{0})=substr(rastreamentovenda.referencia,1,{0}) " +
-
-                         " left join " +
-                         " ( " +
-                         " SELECT p.codigo, SUM(ri.quantidade) as qtd, referencia " +
-                         " FROM retorno r JOIN retornoitem ri ON r.codigo = ri.retorno JOIN acertoconsignado a ON a.codigo = r.acerto " +
-                         " JOIN pessoa p ON p.codigo = a.cliente " +
-                         " WHERE a.dataEfetiva IS NULL " +
-                         " GROUP BY p.codigo, ri.referencia " +
-                         " ) rastreamentoretorno on p.codigo=rastreamentoretorno.codigo " +
-                         " and substr(si.referencia,1,{0})=substr(rastreamentoretorno.referencia,1,{0}) " +
-
-                         " WHERE a.dataEfetiva IS NULL " +
-                         " GROUP BY p.codigo, substr(si.referencia,1,{0}), a.previsao " +
-                         " HAVING saldo > 0 " + 
-                         " ORDER BY SUM(si.quantidade) desc ", TAMANHO_REFERÊNCIA_RASTREÁVEL);
+                    cmd.CommandText = string.Format(
+                            " SELECT saidas.codigopessoa, saidas.nome, saidas.qtd - ifnull(vendas.qtd,0) - ifnull(retornos.qtd,0) as saldo, " +
+                            "  saidas.refrastreavel " +
+                            "  FROM ( " +
+                            "    SELECT p.codigo as codigopessoa, p.nome, a.previsao, sum(si.quantidade) as qtd, substr(referencia,1,{0}) as refrastreavel " +
+                            "    FROM saida s JOIN saidaitem si ON s.codigo = si.saida JOIN acertoconsignado a ON a.codigo = s.acerto " +
+                            "    JOIN pessoa p ON p.codigo = a.cliente " +
+                            "    WHERE a.dataEfetiva IS NULL " +
+                            "    GROUP BY codigopessoa, refrastreavel " +
+                            "  ) saidas " +
+                            "  left join " +
+                            "  ( SELECT p.codigo as codigopessoa, a.previsao, SUM(vi.quantidade) as qtd, substr(referencia,1,{0}) as refrastreavel " +
+                            "  FROM venda v JOIN vendaitem vi ON v.codigo = vi.venda JOIN acertoconsignado a ON a.codigo = v.acerto " +
+                            "  JOIN pessoa p ON p.codigo = a.cliente " +
+                            "  WHERE a.dataEfetiva IS NULL " +
+                            "  GROUP BY codigopessoa, refrastreavel " +
+                            "  ) vendas " +
+                            "  ON  saidas.codigopessoa=vendas.codigopessoa " +
+                            "  and saidas.refrastreavel=vendas.refrastreavel " +
+                            "  left join " +
+                            "  ( SELECT p.codigo as codigopessoa, a.previsao, SUM(ri.quantidade) as qtd, substr(referencia,1,{0}) as refrastreavel " +
+                            "  FROM retorno r JOIN retornoitem ri ON r.codigo = ri.retorno JOIN acertoconsignado a ON a.codigo = r.acerto " +
+                            "  JOIN pessoa p ON p.codigo = a.cliente " +
+                            "  WHERE a.dataEfetiva IS NULL " +
+                            "  GROUP BY codigopessoa, refrastreavel " +
+                            "  ) retornos " +
+                            "  ON  saidas.codigopessoa=retornos.codigopessoa " +
+                            "  and saidas.refrastreavel=retornos.refrastreavel " +
+                            "  HAVING saldo > 0 " +
+                            "  ORDER BY saidas.qtd desc ", TAMANHO_REFERÊNCIA_RASTREÁVEL);
 
                     using (IDataReader leitor = cmd.ExecuteReader())
                     {
@@ -1142,20 +1144,18 @@ namespace Entidades.Mercadoria
                             {
                                 RastroConsignado rastro = new RastroConsignado();
 
-                                rastro.ReferênciaNumérica = leitor.GetString(4);
-
                                 rastro.Pessoa = new Entidades.Pessoa.PessoaCódigoNome(
                                     Convert.ToUInt64(leitor.GetValue(0)),
                                     leitor.GetString(1));
-                                rastro.Devolução = leitor.IsDBNull(2) ? (DateTime?)null : (DateTime?)leitor.GetDateTime(2);
-                                rastro.Quantidade = leitor.GetInt32(3);
+                                rastro.Quantidade = leitor.GetInt32(2);
+                                rastro.ReferênciaRastreável = leitor.GetString(3);
 
                                 // Procura uma lista para adicionar o rastro nela.
                                 List<RastroConsignado> lista = null;
-                                if (!hash.TryGetValue(ObterReferênciaRastreável(rastro.ReferênciaNumérica), out lista))
+                                if (!hash.TryGetValue(ObterReferênciaRastreável(rastro.ReferênciaRastreável), out lista))
                                 {
                                     lista = new List<RastroConsignado>();
-                                    hash[ObterReferênciaRastreável(rastro.ReferênciaNumérica)] = lista;
+                                    hash[ObterReferênciaRastreável(rastro.ReferênciaRastreável)] = lista;
                                 }
 
                                 lista.Add(rastro);
@@ -1221,7 +1221,7 @@ namespace Entidades.Mercadoria
                             {
                                 RastroConsignado rastro = new RastroConsignado();
 
-                                rastro.ReferênciaNumérica = ReferênciaNumérica;
+                                rastro.ReferênciaRastreável = ReferênciaNumérica;
                                 rastro.Pessoa = new Entidades.Pessoa.PessoaCódigoNome(
                                     Convert.ToUInt64(leitor.GetValue(0)),
                                     leitor.GetString(1));
@@ -1353,12 +1353,10 @@ namespace Entidades.Mercadoria
         /// </summary>
         public struct RastroConsignado
         {
-            //public Mercadoria Mercadoria;
-            public string ReferênciaNumérica;
+            public string ReferênciaRastreável;
             public Pessoa.PessoaCódigoNome Pessoa;
             public int Quantidade;
             public DateTime? Devolução;
-
         }
 
         /// <summary>
