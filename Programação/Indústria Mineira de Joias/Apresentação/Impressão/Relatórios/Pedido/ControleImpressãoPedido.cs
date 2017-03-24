6d84008033f734c37c6df6b9f9a66e6ccc1bd4ff
André Pontes <andrepontes@gmail.com>
2017-03-24 10:03:19 -0300

    Refatoração

--- Programação/Indústria Mineira de Joias/Apresentação/Impressão/Relatórios/Pedido/ControleImpressãoPedido.cs
+++ Programação/Indústria Mineira de Joias/Apresentação/Impressão/Relatórios/Pedido/ControleImpressãoPedido.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Entidades.Pessoa;
-using Entidades;
-using CrystalDecisions.CrystalReports.Engine;
-using System.Data;
-using System.Collections;
+﻿using CrystalDecisions.CrystalReports.Engine;
 using Entidades.Mercadoria;
 using Entidades.PedidoConserto;
+using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace Apresentação.Impressão.Relatórios.Pedido
 {
@@ -19,19 +15,44 @@ namespace Apresentação.Impressão.Relatórios.Pedido
             DataTable tabela = ds.Tables["Pedido"];
             DataTable info = ds.Tables["Informações"];
             DataRow item = info.NewRow();
+
             item["dataInicio"] = início.ToShortDateString();
             item["dataFim"] = fim.ToShortDateString();
             item["título"] = apenasConsertos ? "Relação de Consertos" : "Relação de Pedidos";
             item["tipoPeríodo"] = períodoPrevisão ? "Previsão" : "Recebido";
-
             info.Rows.Add(item);
 
-            DataTable tabelaItens = ds.Tables["Itens"];
-            PrepararItens(pedidos, tabelaItens);
+            PrepararItens(pedidos, ds.Tables["Itens"]);
+            AdicionarRastreamento(ds.Tables["Rastro"], Entidades.Mercadoria.Mercadoria.ObterRastreamento());
+            AdicionarPedidosPendentes(pedidos, tabela);
+            relatório.SetDataSource(ds);
+        }
 
-            DataTable tabelaRastro = ds.Tables["Rastro"];
-            Dictionary<string, List<Entidades.Mercadoria.Mercadoria.RastroConsignado>> rastreamentos = Entidades.Mercadoria.Mercadoria.ObterRastreamento();
-            
+        private static void AdicionarPedidosPendentes(List<Entidades.PedidoConserto.Pedido> pedidos, DataTable tabela)
+        {
+            foreach (Entidades.PedidoConserto.Pedido p in ObterPedidosPendentes(pedidos))
+            {
+                DataRow linha = tabela.NewRow();
+                MapearItem(p, linha);
+                tabela.Rows.Add(linha);
+            }
+        }
+
+        private static List<Entidades.PedidoConserto.Pedido> ObterPedidosPendentes(List<Entidades.PedidoConserto.Pedido> pedidos)
+        {
+            List<Entidades.PedidoConserto.Pedido> listaPedidos = new List<Entidades.PedidoConserto.Pedido>();
+
+            foreach (Entidades.PedidoConserto.Pedido pedido in pedidos)
+            {
+                if (!pedido.DataConclusão.HasValue)
+                    listaPedidos.Add(pedido);
+            }
+
+            return listaPedidos;
+        }
+
+        private static void AdicionarRastreamento(DataTable tabelaRastro, Dictionary<string, List<Entidades.Mercadoria.Mercadoria.RastroConsignado>> rastreamentos)
+        {
             foreach (KeyValuePair<string, List<Entidades.Mercadoria.Mercadoria.RastroConsignado>> par in rastreamentos)
             {
                 foreach (Entidades.Mercadoria.Mercadoria.RastroConsignado rastro in par.Value)
@@ -45,100 +66,77 @@ namespace Apresentação.Impressão.Relatórios.Pedido
                     tabelaRastro.Rows.Add(linha);
                 }
             }
-
-
-            List<Entidades.PedidoConserto.Pedido> listaPedidos = new List<Entidades.PedidoConserto.Pedido>();
-
-            foreach (Entidades.PedidoConserto.Pedido pedido in pedidos)
-            {
-                /* Na apresentação, pode-se escolher entre já entregues ou não.
-                 * Porém, apenas os ainda não entregues são obtidos para impressão.
-                 * C:\Users\Andre Pontes\Source\Repos\imjoias\Programação\Indústria Mineira de Joias\Apresentação\Impressão\Relatórios\Pedido\ControleImpressãoPedido.cs
-                 * É necessário filtrar ainda ou já concluidos.
-                 * Só é impresso a pendência, ou seja, os pedidos não concluídos.
-                 */
-
-                if (!pedido.DataConclusão.HasValue)
-                    listaPedidos.Add(pedido);
-            }
-
-            //listaPedidos.Sort();
-            
-            foreach (Entidades.PedidoConserto.Pedido p in listaPedidos)
-            {
-                DataRow linha = tabela.NewRow();
-                MapearItem(p, linha);
-                tabela.Rows.Add(linha);
-            }
-
-            relatório.SetDataSource(ds);
         }
 
         private static void MapearItem(Entidades.PedidoConserto.Pedido pedido, DataRow linha)
         {
             linha["código"] = pedido.Código;
+            linha["tipo"] = ObterTipoPedido(pedido.TipoPedido);
 
-            switch (pedido.TipoPedido)
-            {
-                case Entidades.PedidoConserto.Pedido.Tipo.Conserto:
-                    linha["tipo"] = "Conserto";
-                    break;
-
-                case Entidades.PedidoConserto.Pedido.Tipo.Pedido:
-                    linha["tipo"] = "Pedido";
-                    break;
-
-                default:
-                    throw new NotSupportedException();
-            }
-
-            if (pedido.Cliente != null)
-            {
-                List<Entidades.Pessoa.Endereço.Endereço> endereços =
-                pedido.Cliente.Endereços.ExtrairElementos();
-
-                string cidade = endereços.Count > 0 ? endereços[0].Localidade != null ? endereços[0].Localidade.Nome + endereços[0].Localidade.Estado != null ? " - " + endereços[0].Localidade.Estado.Sigla : "" : "" : "";
-                linha["cliente"] = pedido.Cliente.Nome + " (" + pedido.Cliente.Código.ToString() + ") " + cidade;
-            } else
-                linha["cliente"] = pedido.NomeDoCliente;
-                
+            linha["cliente"] = (pedido.Cliente != null) ?
+                pedido.Cliente.Nome + " (" + pedido.Cliente.Código.ToString() + ") " + ObterCidade(pedido) :
+                pedido.NomeDoCliente;
 
             linha["representante"] = pedido.Representante != null ? Entidades.Pessoa.Pessoa.AbreviarNome(pedido.Representante.Nome) : "";
             linha["receptor"] = pedido.Receptor.PrimeiroNome;
             linha["dataRecepção"] = pedido.DataRecepção;
             linha["dataPrevisão"] = pedido.DataPrevisão;
             linha["observações"] = pedido.Observações;
-            
-            //linha["controle"] = pedido.Controle != null ? pedido.Controle.ToString() : "";
+        }
+
+        private static string ObterCidade(Entidades.PedidoConserto.Pedido pedido)
+        {
+            List<Entidades.Pessoa.Endereço.Endereço> endereços = pedido.Cliente.Endereços.ExtrairElementos();
+            return endereços.Count > 0 ? endereços[0].Localidade != null ? endereços[0].Localidade.Nome + endereços[0].Localidade.Estado != null ? " - " + endereços[0].Localidade.Estado.Sigla : "" : "" : "";
+        }
+
+        private static object ObterTipoPedido(Entidades.PedidoConserto.Pedido.Tipo tipo)
+        {
+            switch (tipo)
+            {
+                case Entidades.PedidoConserto.Pedido.Tipo.Conserto:
+                    return "Conserto";
+
+                case Entidades.PedidoConserto.Pedido.Tipo.Pedido:
+                    return "Pedido";
+
+                default:
+                    throw new NotSupportedException();
+            }
         }
 
         public static void PrepararItens(List<Entidades.PedidoConserto.Pedido> pedidos, DataTable tabelaItens)
         {
             Dictionary<string, MercadoriaFornecedor> fornecedores = MercadoriaFornecedor.ObterFornecedores();
             List<PedidoItem> itens = PedidoItem.Obter(pedidos);
-            foreach (PedidoItem p in itens)
+
+            foreach (PedidoItem item in itens)
             {
-                DataRow linha = tabelaItens.NewRow();
-                linha["código"] = p.Código;
-                linha["pedido"] = p.Pedido;
-                linha["quantidade"] = p.Quantidade;
-                linha["referênciaFormatada"] = p.ReferênciaFormatada;
-                linha["referênciaNumérica"] = p.ReferênciaNumérica;
-                linha["referênciaRastreável"] = Entidades.Mercadoria.Mercadoria.ObterReferênciaRastreável(p.ReferênciaNumérica);
-
-                if (!String.IsNullOrEmpty(p.Descrição))
-                    linha["descrição"] = p.Descrição.Replace("\r\n"," ");
-
-                MercadoriaFornecedor fornecedor = null;
-                if ((p.ReferênciaNumérica != null) && fornecedores.TryGetValue(p.ReferênciaNumérica, out fornecedor))
-                {
-                    linha["fornecedor"] = fornecedor.FornecedorCódigo.ToString();
-                    linha["refFornecedor"] = fornecedor.ReferênciaFornecedorComFFL;
-                }
-
-                tabelaItens.Rows.Add(linha);
+                AdicionarItem(tabelaItens, fornecedores, item);
             }
         }
 
+        private static void AdicionarItem(DataTable tabelaItens, Dictionary<string, MercadoriaFornecedor> hashFornecedores, PedidoItem item)
+        {
+            DataRow linha = tabelaItens.NewRow();
+            linha["código"] = item.Código;
+            linha["pedido"] = item.Pedido;
+            linha["quantidade"] = item.Quantidade;
+            linha["referênciaFormatada"] = item.ReferênciaFormatada;
+            linha["referênciaNumérica"] = item.ReferênciaNumérica;
+            linha["referênciaRastreável"] = Entidades.Mercadoria.Mercadoria.ObterReferênciaRastreável(item.ReferênciaNumérica);
+
+            if (!String.IsNullOrEmpty(item.Descrição))
+                linha["descrição"] = item.Descrição.Replace("\r\n", " ");
+
+            MercadoriaFornecedor fornecedor = null;
+            if ((item.ReferênciaNumérica != null) && hashFornecedores.TryGetValue(item.ReferênciaNumérica, out fornecedor))
+            {
+                linha["fornecedor"] = fornecedor.FornecedorCódigo.ToString();
+                linha["refFornecedor"] = fornecedor.ReferênciaFornecedorComFFL;
+            }
+
+            tabelaItens.Rows.Add(linha);
+        }
     }
 }
