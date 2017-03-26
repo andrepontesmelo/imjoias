0cc9dd65d4612e4fc4f57ef4eba19038e531bf76
André Pontes <andrepontes@gmail.com>
2017-03-26 15:30:14 -0300

    Referência rastreável deve ser formatada - RED

---
+++ Programação/Indústria Mineira de Joias/EntidadesTests/Mercadoria/MercadoriaTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Entidades.Mercadoria.Tests
+{
+    [TestClass()]
+    public class MercadoriaTests
+    {
+        [TestMethod()]
+        public void DeveMascararReferência11Dígitos()
+        {
+            Assert.AreEqual("999.000.00.021", Mercadoria.MascararReferência("99900000021"));
+        }
+
+        [TestMethod()]
+        public void DeveMascararReferência8Dígitos()
+        {
+            Assert.AreEqual("123.456.78", Mercadoria.MascararReferência("12345678"));
+        }
+
+        [TestMethod()]
+        public void NãoDeveMascararReferência7Dígitos()
+        {
+            Assert.AreEqual("1234567", Mercadoria.MascararReferência("1234567"));
+        }
+    }
+}
